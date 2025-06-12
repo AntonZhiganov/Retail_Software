9dc4ed1b14572ca849b912045ebc05223635f223
Anton Zhiganov <Antohajigan@gmail.com>
2025-06-12 11:40:51 +0200

    I created the initial form, added buttons, and connected them, but for now they only show text.

---
+++ Retail_Software/mainwindow.cpp
@@ -0,0 +1,46 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_purchaseButton_clicked()
+{
+
+    QMessageBox::information(this, "Purchase", "'Purchase' pressed");
+}
+void MainWindow::on_additionalExpensesButton_clicked()
+{
+
+    QMessageBox::information(this, "Additional expenses", "'Additional expenses' pressed");
+}
+void MainWindow::on_saleButton_clicked()
+{
+
+    QMessageBox::information(this, "Sale", "'Sale' pressed");
+}
+void MainWindow::on_checkButton_clicked()
+{
+
+    QMessageBox::information(this, "Check warehouse", "'Check warehouse' pressed");
+}
+void MainWindow::on_incomeAndExpensesButton_clicked()
+{
+
+    QMessageBox::information(this, "Income and expenses", "'Information about income and expenses' pressed");
+}
+void MainWindow::on_clientsButton_clicked()
+{
+
+    QMessageBox::information(this, "Information about clients", "'Information about clients' pressed");
+}
---
+++ Retail_Software/mainwindow.h
@@ -0,0 +1,38 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+
+private slots:
+    void on_purchaseButton_clicked();
+private slots:
+    void on_additionalExpensesButton_clicked();
+private slots:
+    void on_saleButton_clicked();
+private slots:
+    void on_checkButton_clicked();
+private slots:
+    void on_incomeAndExpensesButton_clicked();
+private slots:
+    void on_clientsButton_clicked();
+};
+
+
+#endif // MAINWINDOW_H
